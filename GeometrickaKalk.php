dbbdced9120b5d751e2fb4884468874ff3ea025b
RobertGlos <glos.robert@seznam.cz>
2023-01-25 10:14:49 +0100

    Update GeometrickaKalk.php

--- GeometrickaKalk.php
+++ GeometrickaKalk.php
@@ -1,3 +1,31 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <form action="index.php" method="post">
+      <input type="number" name = "a" placeholder="Napište stranu a">
+      <br>
+      <input type="hidden" id="1" name = "b" placeholder="Napiste stranu b">
+      <br>
+      <input type="hidden" id="3" name = "c" placeholder="Napiste stranu c">
+      <br>
+      <input type="submit" value="Vypocitat">
+    </form>
+    <br>
+    <button id="2" onclick="showHidden(1)">+ Strana</button>
+    <button id="4" onclick="showHidden(3)">+ strana</button>
+    <script>
+      document.getElementById(4).style.visibility = 'hidden'
+      function showHidden(a){
+        document.getElementById(a).type="number"
+        document.getElementById(a+1).remove()
+        document.getElementById(a+3).style.visibility = 'visible';
+      }
+    </script>
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
 <?php
 class geoCalc{
   public $a;
@@ -12,24 +40,26 @@ class geoCalc{
     if ($this->c==null){
       //ctver
       if ($this->b == null){
-        return $this->a*$this->a;
+        return "Obsah Ctverce je: ". $this->a*$this->a;
       }
         //obd
       else{
-        return $this->a * $this->b;
+        return "Obsah Obdelniku je: " . $this->a * $this->b;
       }
     }
       //Trojuhl
     elseif($this->a == $this->b){
-      return (($this->c * ((($this->a ** 2)-(($this->c/2) ** 2))**0.5)) / 2);
+      return "Obsah Trojuhelniku je: ". (($this->c * ((($this->a ** 2)-(($this->c/2) ** 2))**0.5)) / 2);
       }
       
     else{
       $s = ($this->a + $this->b + $this->c)/2;
-      return ($s*($s - $this->a)*($s - $this->b)*($s - $this->c))**(1/2);
+      return "Obsah Trojuhelniku je: ". ($s*($s - $this->a)*($s - $this->b)*($s - $this->c))**(1/2);
     }
   }
   
 }
-$troj = new geoCalc(6,5,8);
-echo $troj->calc();
+if(isset($_POST["a"])){
+  $geoObr = new geoCalc($_POST["a"], $_POST["b"], $_POST["c"]);
+  echo $geoObr->calc();
+}
